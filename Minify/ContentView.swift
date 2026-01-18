da8533f9b370eb89222b011c2ba775441dcb549f
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2026-01-18 22:39:36 +0600

    Initial Commit

---
+++ Minify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Minify
+//
+//  Created by Hafizur Rahman on 18/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Minify/MinifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinifyApp.swift
+//  Minify
+//
+//  Created by Hafizur Rahman on 18/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MinifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
